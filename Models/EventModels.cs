8a93bada97decdfd10673ef33f982ccb0ec8cae2
Supun Subodha <supunmalsingha@gmail.com>
2026-07-13 11:16:50 +0530

    Added input validation

--- Models/EventModels.cs
+++ Models/EventModels.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EventEase.Models
 {
     public class EventModels
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "An event name is absolutely required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters.")]
         public string Name { get; set; } = string.Empty;
         public DateTime Date { get; set; }
+        [Required(ErrorMessage = "You must specify a location.")]
         public string Location { get; set; } = string.Empty;
     }
 }
